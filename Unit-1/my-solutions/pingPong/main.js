4320c32f1e389539fdebf69a3c8a9ab093871cc4
elvece <lvcifferello@gmail.com>
2015-09-17 10:18:26 -0600

    switch serve between players functionality working

--- Unit-1/my-solutions/pingPong/main.js
+++ Unit-1/my-solutions/pingPong/main.js
@@ -1,22 +1,54 @@
 var app = angular.module('pingPong', []);
 
 var mainController = function($scope){
-  $scope.player_one_score = 0;
-  $scope.player_two_score = 0;
+  $scope.init = function(){
+    $scope.game = {
+      playerOne: {
+        score: 0,
+        serving: true
+      },
+      playerTwo: {
+        score: 0,
+        serving: false
+      }
+    };
+  };
+  $scope.init();
+  $scope.click = 0;
+
   $scope.incrementScorePlayerOne = function(){
-    if ($scope.player_one_score < 11){
-      $scope.player_one_score ++;
+    if ($scope.game.playerOne.score < 11){
+      $scope.game.playerOne.score ++;
+      $scope.click ++;
     }
   };
+
   $scope.incrementScorePlayerTwo = function(){
-    if ($scope.player_two_score < 11){
-      $scope.player_two_score ++;
+    if ($scope.game.playerTwo.score < 11){
+      $scope.game.playerTwo.score ++;
+      $scope.click ++;
+    }
+  };
+  $scope.serving = function(){
+    if ($scope.click === 0){
+      $scope.game.playerOne.serving = true;
+    }
+    if ($scope.click % 2 === 0){
+      if ($scope.game.playerOne.serving){
+        $scope.game.playerOne.serving = false;
+        $scope.game.playerTwo.serving = true;
+      } else {
+        $scope.game.playerOne.serving = true;
+        $scope.game.playerTwo.serving = false;
+      }
     }
   };
 };
 
 app.controller('mainController', ['$scope', mainController]);
 
-//on click, need to incremement score by 1
-//when reach 11, game over, swtich winner background to green and loser to red
-//display current server, which switches after every two points
+//TO DO:
+//if player1 score is 11, then disable click of player2
+
+
+
