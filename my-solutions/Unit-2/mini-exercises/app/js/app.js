1620141f2c3e3e0b276f67bbba115a8dd5b5a74a
elvece <lvcifferello@gmail.com>
2015-10-01 12:29:29 -0600

    added partial files and routes, working to display active page

--- my-solutions/Unit-2/mini-exercises/app/js/app.js
+++ my-solutions/Unit-2/mini-exercises/app/js/app.js
@@ -1,1 +1,30 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'homeController'
+    })
+    .when( '/bio', {
+      templateUrl: 'partials/bio.html',
+      controller: 'bioController'
+    })
+    .when( '/resume', {
+      templateUrl: 'partials/resume.html',
+      controller: 'resumeController'
+    })
+    .when( '/projects', {
+      templateUrl: 'partials/projects.html',
+      controller: 'projectsController'
+    })
+    .when( '/add', {
+      templateUrl: 'partials/add.html',
+      controller: 'addThingsController'
+    })
+    .when( '/add/:num1/:num2', {
+      templateUrl: 'partials/add.html',
+      controller: 'addController'
+    })
+   .otherwise({ redirectTo: '/'});
+ });
--- my-solutions/Unit-2/mini-exercises/app/js/controllers.js
+++ my-solutions/Unit-2/mini-exercises/app/js/controllers.js
@@ -1,3 +1,36 @@
-app.controller('myController', function($scope) {
-  $scope.greeting = "Hello World!";
+app.controller('HeaderController', function($scope, $location){
+    $scope.isActive = function (viewLocation) {
+        return $location.path().indexOf(viewLocation) === 0;
+    };
 });
+
+app.controller('homeController', function($scope) {
+  $scope.greeting = 'This is the homepage.';
+});
+
+app.controller('projectsController', function($scope) {
+  $scope.greeting = 'These are my projects.';
+
+});
+
+app.controller('bioController', function($scope) {
+  $scope.greeting = 'This is my bio.';
+});
+
+app.controller('resumeController', function($scope) {
+  $scope.greeting = 'This is my resume.';
+
+});
+
+app.controller('addThingsController', function($scope, $routeParams){
+  $scope.greeting = 'Add two numbers to url to add stuff.';
+  $scope.ifParams = false;
+});
+
+app.controller('addController', function($scope, $routeParams){
+  $scope.num1 = parseInt($routeParams.num1);
+  $scope.num2 = parseInt($routeParams.num2);
+  $scope.ifParams = true;
+});
+
+
